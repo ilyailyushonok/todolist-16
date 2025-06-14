47f531b4a73927142d5f4f14f3c490597eb53e12
ilya ilyushonok <ilyushonok2014@mail.ru>
2025-06-14 16:45:10 +0300

    PageNotFound link redirect

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,12 @@
 import styles from "./PageNotFound.module.css"
+import { Link } from "react-router"
+import { Path } from "@/common/routing/Routing.tsx"
+import Button from '@mui/material/Button'
 
 export const PageNotFound = () => (
-  <>
+  <div className={styles.contain}>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
-  </>
+    <Button children={<Link to={Path.Main} className={styles.redirectBtn}>На главную</Link>}/>
+  </div>
 )
